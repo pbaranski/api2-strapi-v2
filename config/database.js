95ce047dd285ac4d8cbcb1d3f569df1f6ab5c33c
Przemek B <przemek@jaktestowac.pl>
2022-01-17 13:43:34 +0100

    Added support for PG on Heroku and local

--- config/database.js
+++ config/database.js
@@ -1,11 +1,59 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+if(process.env.DATABASE_URL){
+  const parse = require('pg-connection-string').parse;
+  const config = parse(process.env.DATABASE_URL);
+  module.exports = ({ env }) => ({
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: 'postgres',
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      debug: false,
     },
-    useNullAsDefault: true,
-  },
-});
+  });
+} else {
+  module.exports = ({ env }) => ({
+    connection: {
+      client: 'sqlite',
+      connection: {
+        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      },
+      useNullAsDefault: true,
+    },
+  });
+}
+
+
+// module.exports = ({ env }) => {
+//   // 
+//       console.log(parse(env("DATABASE_URL"));
+//       const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  
+//       return {
+//         defaultConnection: "default",
+//         connections: {
+//           default: {
+//             connector: "bookshelf",
+//             settings: {
+//               client: "postgres",
+//               host,
+//               port,
+//               database,
+//               username: user,
+//               password,
+//               ssl: { rejectUnauthorized: false },
+//             },
+//             options: {},
+//           },
+//         },
+//       };
+//   // }
+// };
